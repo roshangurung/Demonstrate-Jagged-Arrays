b52c6e5eed96cec0e56e553d911afaee9a3b5955
roshangurung <r.gurung1992@gmail.com>
2019-12-10 19:25:26 -0800

    Initial commit

---
+++ Demonstrate Jagged Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Demonstrate_Jagged_Arrays
+{
+    class Program//////This C# Program is used to Demonstrate Jagged Arrays. The elements of a jagged array can be of different dimensions and sizes. A jagged array is sometimes called an “array of arrays.” Create a new array in the jagged array, and assign its coefficient values, and form second row and third row by using array initialize.///
+    {
+        static void Main(string[] args)
+        {
+            int[][] jag = new int[3][];
+            jag[0] = new int[2];
+            jag[0][0] = 11;
+            jag[0][1] = 12;
+            jag[1] = new int[1] { 11 };
+            jag[2] = new int[3] { 14, 15, 16 };
+            for (int i = 0; i < jag.Length; i++)
+            {
+                int[] innerArray = jag[i];///Here Jagged Arrays can store efficiently many rows of varying lengths. Any type of data, reference or value, can be used. For loop is used to print the jagged array element values///
+                for (int a = 0; a < innerArray.Length; a++)
+                {
+                    Console.WriteLine(innerArray[a] + " ");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
+///Output: 
+/// 11,12,11,14,15,16
